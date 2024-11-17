c356e0c46175909ccdb2957496fb14c538aa0ff3
paggadev <paggawallet@gmail.com>
2024-11-17 01:03:45 +0100

    upd addresses

--- miniapp/src/config/contracts.ts
+++ miniapp/src/config/contracts.ts
@@ -6,19 +6,16 @@ export const devContracts: Record<number, { claim: `0x${string}`}> = {
     claim: "0x6540c58a7236df1dc9ff563d4a75960f32926fe2",
   },
   [celoAlfajores.id]: {
-    claim: "0xE47845dac04A7C91E73B8f09b441785810D40f69", // Replace with the actual Celo contract address
+    claim: "0xE47845dac04A7C91E73B8f09b441785810D40f69",s
   },
   [gnosisChiado.id]: {
-    claim: "0x7cFD096A93dD589fdB94F168158a6c631fFc22c8", // Replace with the actual Gnosis contract address
+    claim: "0x7cFD096A93dD589fdB94F168158a6c631fFc22c8",
   },
   [hederaTestnet.id]: {
-    claim: "0x7cfd096a93dd589fdb94f168158a6c631ffc22c8", // Replace with the actual Hedera contract address
+    claim: "0x7cfd096a93dd589fdb94f168158a6c631ffc22c8",
   },
   [rootstockTestnet.id]: {
-    claim: "0x7cfd096a93dd589fdb94f168158a6c631ffc22c8", // Replace with the actual Rootstock contract address
-  },
-  [lineaTestnet.id]: {
-    claim: "0xLineaClaimAddress", // Replace with the actual Linea contract address
+    claim: "0x7cfd096a93dd589fdb94f168158a6c631ffc22c8", 
   },
   [flowTestnet.id]: {
     claim: "0xbefC6F3B404F2BF6B4c52E05c55BE15ee3Fe294d",
@@ -51,19 +48,19 @@ export const devContracts: Record<number, { claim: `0x${string}`}> = {
     claim: "0xbefC6F3B404F2BF6B4c52E05c55BE15ee3Fe294d",
   },
   [base.id]: {
-    claim: "0xa7bc316fa35c9eac97bd8f05fe6404ffda826308", // Replace with the actual Base contract address
+    claim: "0xa7bc316fa35c9eac97bd8f05fe6404ffda826308", 
   },
   [80084]: {
-    claim: "0xaC0d56E871DbeecE3e576A4B64183eCd9a479195", // Replace with the actual Base contract address
+    claim: "0xaC0d56E871DbeecE3e576A4B64183eCd9a479195", 
   },
 };
 
 const prodContracts: Record<number, { claim: `0x${string}`}> = {
   [base.id]: {
-    claim: "0xa7bc316fa35c9eac97bd8f05fe6404ffda826308", // Replace with the actual Base contract address
+    claim: "0xa7bc316fa35c9eac97bd8f05fe6404ffda826308", 
   },
   [80084]: {
-    claim: "0xaC0d56E871DbeecE3e576A4B64183eCd9a479195", // Replace with the actual Base contract address
+    claim: "0xaC0d56E871DbeecE3e576A4B64183eCd9a479195",
   },
   // Add other production chain contracts
 }
